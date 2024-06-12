d79b6a1f43863d0c2d5925da0610cacbb884db4e
alazaradane <alazargithub369@gmail.com>
2024-06-12 08:10:09 +0300

    schadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "./components/ui/button"
 
 
 const App = () => {
   return (
-    <div>
+    <div className=" ml-10">
       <h1 className=" text-center text-3xl font-bold">Hello World!</h1>
+      <Button>Click me</Button>
     </div>
   )
 }
