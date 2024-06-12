a382a77b57352d90ba2eb1eebe9a8169240ec391
alazaradane <alazargithub369@gmail.com>
2024-06-12 18:33:38 +0300

    adding responsivness to navBar comp

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+
 import { Link } from "react-router-dom";
 import { logo } from "../assets/icons";
 import { menu } from "../assets/images";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../components/ui/dropdown-menu"
+import { navItems } from "../data";
+
 
 const NavBar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const handleMenuToggle = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+  
 
   return (
     <section className="relative flex items-center mb-5 p-4 lg:p-0">
@@ -20,37 +26,34 @@ const NavBar = () => {
             <span className="text-secondary-blue">TECH</span>OPIA
           </p>
         </div>
-        {/* Mobile Menu Button */}
-        <div className="lg:hidden">
-          <img src={menu} alt="humburger" onClick={handleMenuToggle} className=" w-5 h-5 m-3 cursor-pointer"/>
+        <div className="lg:hidden bg-primary">
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <img src={menu} alt="humburger" className=" w-5 h-5 m-3 cursor-pointer"/>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Explore</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              {navItems.map((item)=>(
+                <DropdownMenuItem key={item.name}>
+                  <Link to={item.link}>{item.name}</Link>
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+
         </div>
       </div>
 
-      {/* Main Links */}
-      <div className={`hidden lg:flex items-center ml-0 lg:ml-[20rem] mb-4 gap-4 lg:gap-[2rem] font-poppins font-semibold text-md text-secondary`}>
-        <Link to="/">Home</Link>
-        <Link to="/project">Projects</Link>
-        <Link to="/blog">Blogs</Link>
-        <Link to="/event">Events</Link>
+      <div className={`hidden lg:flex items-center ml-0 lg:ml-[10rem] mb-4 gap-4 lg:gap-[2rem] font-poppins font-semibold text-md text-secondary`}>
+        {navItems.map((item)=>(
+          <Link key={item.name} to={item.link}>{item.name}</Link>
+        ))}
       </div>
-
-      {/* Contact Us Link */}
       <div className="hidden lg:flex items-center ml-0 lg:ml-[5rem] mb-4 font-poppins font-semibold text-md bg-white rounded-[2rem] px-5 py-2">
         <Link to="/contact_us">Contact Us</Link>
       </div>
 
-      {/* Mobile Menu */}
-      {isMenuOpen && (
-        <div className="lg:hidden w-full mt-4">
-          <div className="flex flex-col items-center gap-4 font-poppins font-semibold text-md text-secondary">
-            <Link to="/">Home</Link>
-            <Link to="/project">Projects</Link>
-            <Link to="/blog">Blogs</Link>
-            <Link to="/event">Events</Link>
-            <Link to="/contact_us" className="bg-white rounded-[2rem] px-5 py-2">Contact Us</Link>
-          </div>
-        </div>
-      )}
     </section>
   );
 };
---
+++ src/data/index.ts
@@ -0,0 +1,9 @@
+
+export const navItems = [
+    {name: 'Home', link:'/'},
+    {name: 'Projects', link:'/projects:id'},
+    {name: 'Blogs', link:'/blogs:id'},
+    {name: 'Events', link:'/events:id'},
+    {name:"Services", link:'#service'},
+    {name:"About Us", link:'#about_us'}
+]
